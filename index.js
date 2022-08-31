fff4480a520da5a0193694dc1166b8b71888c2fb
Delmar <reliablesource13@gmail.com>
2022-08-31 15:25:34 -0700

    Update stuff
    Did stuff

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+let formData;
 
 const app = express();
 app.use(express.static('public'));
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body);
+    formData = req.body;
     res.sendFile(path.resolve(__dirname, 'form.html'));
+    console.log(`Type: ${typeof(formData)} || Object: ${formData.username}, ${formData.password}`);
 });
